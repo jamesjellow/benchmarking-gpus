66db316d3f6b4d7755e2b6debdf5b09d8c99f1b8
Luis <lwchave2@illinois.edu>
2024-12-12 22:17:53 -0600

    Revised submission process to feed into API gateway and added optional leaderboard tracking submission per PROFs request.

--- inference/bert99/submit-results.sh
+++ inference/bert99/submit-results.sh
@@ -1,13 +1,5 @@
-# PLEASE ENSURE YOU GRAB THE AWS LINUX USER CREDENTIALS AND SET THEM
-# https://uillinoisedu.sharepoint.com/:t:/s/CS598ResearchGroup79/EeGvJadKOgFBov5_1lE8AfQBDRLlrbJpzwLi6Kyxl3oudg?e=oL85da
+# TODO GRAB API KEY FROM???
+# TODO GRAB API KEY FROM???
 
-sudo apt install unzip uuid -y
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
-YOUR_RESULTS_DIR=$HOME/runs/
-uuid=$(uuidgen -t)
-
-aws s3 sync ${YOUR_RESULTS_DIR} s3://benchmarking-gpus/inference/bert99/results/${uuid}/runs
+python3 ${HOME}/submit_results.py "runs"
 echo submission complete!
